b9b09b4532ed2542c38cd8b744e1709af1515488
Mohammad <moh.ebrahimi2000@gmail.com>
2023-06-18 17:47:54 +0330

    Add persian support to react and tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [ 'Vazirmatn', 'sans-serif' ],
+      },
+    },
   },
   plugins: [],
 }
